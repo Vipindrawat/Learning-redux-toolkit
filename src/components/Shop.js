5b62b2eb15c5ca117aca1ac8fcbb6c2255f9a926
Vipindrawat <vipinrawat7500@gmail.com>
2023-11-06 08:38:36 +0530

    readme file

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+
+  return (
+    <div className='container my-4'>
+      <button type="button" className="btn btn-primary">-</button>
+      <button type="button" className="btn btn-secondary">add and deposit</button>
+      <button type="button" className="btn btn-primary">+</button>
+    </div>
+  )
+}
+
+export default Shop;
